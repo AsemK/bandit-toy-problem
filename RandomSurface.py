73ccadf8ab0879822522a844a497f730e37b073a
AsemK <asem.khattab@gmail.com>
2018-03-26 16:25:19 +0200

    Rewrote random_positive_definite_mat. Now it can scale up to any dimension. The ranges are still not satisfied in the output of the function.

--- RandomSurface.py
+++ RandomSurface.py
@@ -1,17 +1,18 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
 
 class RandomSurface:
-    def __init__(self, pos_ranges, values_range=(0.0, 1.0), cov_diag_range=(0, 0.01), cov_off_diag_range=(-0.01, 0.01)):
+    def __init__(self, pos_ranges, values_range=(0.0, 1.0), cov_diag_range=(0, 0.1), cov_off_diag_range=(-1, 1)):
         self.pos_ranges = pos_ranges
         self.values_range = values_range
         self.cov_diag_range = cov_diag_range
         self.cov_off_diag_range = cov_off_diag_range
 
         self.rocks = []
-        self.get_surface_at = self.get_continuous_surface_at
+        self.get_surface_at = self.get_smooth_surface_at
 
     def get_dim(self):
         return len(self.pos_ranges)
@@ -23,13 +24,14 @@ class RandomSurface:
         return self.values_range
 
     @staticmethod
-    def random_positive_definite_mat(dim, cov_diag_range, cov_off_diag_range):
-        cov = np.random.random([dim, dim]) * (cov_off_diag_range[1]-cov_off_diag_range[0])+cov_off_diag_range[0]
-        cov = (cov + cov.transpose())/2
-        while not np.all(np.linalg.eigvals(cov) > 0):
-            cov = (np.diag(np.random.random(dim))*(cov_diag_range[1]-cov_diag_range[0])+cov_diag_range[0]) \
-                  + np.tril(cov, -1) + np.tril(cov, -1).transpose()
-        return cov
+    def random_positive_definite_mat(dim, diag_range, off_diag_range):
+        Q = np.zeros([dim, dim])
+        while np.linalg.matrix_rank(Q) < dim:
+            Q = np.random.random([dim, dim])*(off_diag_range[1]-off_diag_range[0])+off_diag_range[0]
+        M = np.zeros([dim, dim])
+        while not np.all(np.linalg.eigvals(M) > 0):
+            M = (Q.transpose().dot(np.diag(np.random.random(dim)*(diag_range[1]-diag_range[0])+diag_range[0]))).dot(Q)
+        return M
 
     def random_gaussian_surface(self):
         mean_pos = [np.random.random()*(self.pos_ranges[i][1]-self.pos_ranges[i][0])+self.pos_ranges[i][0]
@@ -38,15 +40,15 @@ class RandomSurface:
         rv = multivariate_normal(mean_pos, cov)
         return rv, mean_pos, cov
 
-    def create_random_rough_surface(self, rocks):
+    def create_random_surface(self, rocks):
         heights = np.random.random(rocks) * (self.values_range[1] - self.values_range[0]) + self.values_range[0]
         heights *= self.values_range[1]/max(heights)
         self.rocks = []
         for i in range(rocks):
             self.rocks.append((*self.random_gaussian_surface(), heights[i]))
 
-    def get_continuous_surface_at(self, pos):
-        result = np.zeros(len(pos))
+    def get_rough_surface_at(self, pos):
+        result = 0
         for rock in self.rocks:
             result = np.maximum(result, rock[0].pdf(pos)/rock[0].pdf(rock[1])*rock[-1])
         return result
@@ -62,7 +64,7 @@ class RandomSurface:
         if self.get_dim() < 1 or self.get_dim() < axis:
             return
 
-        x = np.mgrid[self.pos_ranges[axis][0]:self.pos_ranges[axis][1]:precision]
+        x = np.arange(*self.pos_ranges[axis], precision)
         pos = np.zeros((len(x), self.get_dim()))
         pos[:, axis] = x
         c = 0
@@ -82,8 +84,7 @@ class RandomSurface:
         if self.get_dim() < 2 or self.get_dim() < axis1 or self.get_dim() < axis2:
             return
 
-        x, y = np.mgrid[self.pos_ranges[axis1][0]:self.pos_ranges[axis1][1]:precision,
-                        self.pos_ranges[axis2][0]:self.pos_ranges[axis2][1]:precision]
+        x, y = np.meshgrid(np.arange(*self.pos_ranges[axis1], precision), np.arange(*self.pos_ranges[axis2], precision))
         pos = np.empty(x.shape + (self.get_dim(),))
         c = 0
         for i in range(self.get_dim()):
@@ -96,11 +97,19 @@ class RandomSurface:
                 c += 1
 
         plt.figure()
-        plt.contourf(x, y, self.get_surface_at(pos), levels = np.arange(*self.get_values_range(), 0.1))
+        plt.contourf(x, y, self.get_surface_at(pos), levels=np.arange(*self.get_values_range(), 0.1))
+        plt.axis('square')
+        plt.axis([*self.pos_ranges[axis1], *self.pos_ranges[axis2]])
         plt.show()
 
 
 if __name__ == '__main__':
-    random_surface = RandomSurface([(0.0, 1.0), (0.0, 1.0)])
-    random_surface.create_random_rough_surface(25)
-    random_surface.contour(0, 1, [])
+    # random_surface = RandomSurface([(0.5, 1.5), (0, 2.0), (-1, 1)])
+    # random_surface.create_random_surface(25)
+    # random_surface.contour(1, 2, [1.0])
+    # random_surface.graph(1, [1, 0])
+    dig_range = (0, 1)
+    off_diag_range = (-1, 1)
+    print(RandomSurface.random_positive_definite_mat(1, dig_range, off_diag_range))
+    print(RandomSurface.random_positive_definite_mat(2, dig_range, off_diag_range))
+    print(RandomSurface.random_positive_definite_mat(3, dig_range, off_diag_range))
